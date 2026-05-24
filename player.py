2ee882dfb6952612f52b52779773d57b8fba5495
JanezFabijan <janez.fabijan15@gmail.com>
2026-05-24 21:24:52 +0200

    fix rotate function

--- player.py
+++ player.py
@@ -23,7 +23,7 @@ class Player(CircleShape):
     
 
     def rotate(self, dt):
-        self.rotation = PLAYER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
     
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
